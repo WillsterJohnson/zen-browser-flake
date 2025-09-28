82ba189a86e1a846c858dd718affadb08ae9247d
WillsterJohnson <85991558+WillsterJohnson@users.noreply.github.com>
2025-09-28 15:07:58 +0000

    GitHub Action automated Zen update

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.16.2b";
+      version = "1.16.1b";
       downloadUrl = {
         specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        specific.sha256 = "1nf57xxb8nfir11vq9ixxd7ywf79pgn9yi1irmjarbvqsdqy24mz";
+        specific.sha256 = "04bb9f09kb0bmbgas5yb2qxnjh4gclq3bxnrzj1lijmf3w4240gd";
 
         generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        generic.sha256 = "1nf57xxb8nfir11vq9ixxd7ywf79pgn9yi1irmjarbvqsdqy24mz";
+        generic.sha256 = "04bb9f09kb0bmbgas5yb2qxnjh4gclq3bxnrzj1lijmf3w4240gd";
       };
 
       pkgs = import nixpkgs {
